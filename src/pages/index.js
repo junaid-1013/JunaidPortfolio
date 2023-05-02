f5845b19bd2a848e2d15e6b0bd852084e980ca34
Junaid Ali Bhatti <junaid.ali101452@gmail.com>
2023-05-03 00:36:22 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         <Layout className='pt-0'>
           <div className='flex items-center justify-between w-full'>
             <div className='w-1/2'>
-              <Image src={profilePic} alt="Junaid" className="w-full h-auto" />
+              <Image src={profilePic} alt="Junaid" className="max-w-xl h-auto rounded-full" />
             </div>
             <div className='w-1/2 flex flex-col items-center self-center'>
               <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6xl !text-left' />
