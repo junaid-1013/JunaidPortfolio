f4575cce205ba431bf3c2cbb5b6fc867f794aa19
Junaid Ali Bhatti <junaid.ali101452@gmail.com>
2023-05-12 21:42:06 +0500

    Fully desktop responsive

--- src/components/Education.js
+++ src/components/Education.js
@@ -13,7 +13,7 @@ const Details = ({ type,time,place,info }) => {
             transition={{duration:0.5 , type:"spring"}}
             >
                 <h3 className="capitalize font-bold text-2xl">{type} </h3>
-                <span className="capitalize font-medium text-dark/75 ">
+                <span className="capitalize font-medium text-dark/75 dark:text-light/75">
                     {time} | {place}
                 </span>
                 <p className="font-medium w-full">
@@ -35,12 +35,12 @@ const Education = () => {
     return (
         <div className="my-64">
             <h2 className="font-bold text-8xl mb-32 w-full text-center">
-                Experience</h2>
+                Education</h2>
 
             <div ref={ref} className="w-[75%] mx-auto relative ">
                 <motion.div 
                 style={{scaleY: scrollYProgress}}
-                className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top" />
+                className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light" />
                 <ul className="w-full flex flex-col items-start justify-between ml-4">
                     <Details
                         type="Bachelor of Science In Computer Science" 
--- src/pages/about.js
+++ src/pages/about.js
@@ -40,12 +40,12 @@ const about = () => {
                 <title>Junaid Ali | About Page</title>
                 <meta name='junaid ali' content="myintroduction" />
             </Head>
-            <main className="flex w-full flex-col items-center justify-center">
+            <main className="flex w-full flex-col items-center justify-center dark:text-light">
                 <Layout className="pt-16">
                     <AnimatedText text="Passion Fuels Purpose!" className="mb-16" />
                     <div className="grid w-full grid-cols-8 gap-16">
                         <div className="col-span-3 flex flex-col items-start justify-start">
-                            <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+                            <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
                                 Biography
                             </h2>
                             <p className="font-medium">
@@ -63,10 +63,15 @@ const about = () => {
                             </p>
                         </div>
                         <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark
-                        bg-light p-8
+                        bg-light p-8 dark:bg-dark dark:border-light
                         ">
-                            <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark " />
-                            <Image src={profilePic} alt="Junaid Ali" className='w-full h-auto rounded-2xl' />
+                            <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
+                            <Image src={profilePic} alt="Junaid Ali" className='w-full h-auto rounded-2xl' 
+                            priority
+                             sizes="(max-width: 768px) 100vm,
+                             (max-width:1200px) 50vm,
+                              33vm"
+                            />
                         </div>
 
                         <div className="col-span-2 flex flex-col items-end justify-between">
@@ -74,21 +79,21 @@ const about = () => {
                                 <span className="inline-block text-7xl font-bold">
                                     <AnimateNumber value={50} />+
                                 </span>
-                                <h2 className="text-xl font-medium capitalize text-dark/75">satisfied clinets</h2>
+                                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">satisfied clinets</h2>
                             </div>
 
                             <div className="flex flex-col items-end justify-center">
                                 <span className="inline-block text-7xl font-bold">
                                     <AnimateNumber value={10} />+
                                 </span>
-                                <h2 className="text-xl font-medium capitalize text-dark/75">projects completed</h2>
+                                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">projects completed</h2>
                             </div>
 
                             <div className="flex flex-col items-end justify-center">
                                 <span className="inline-block text-7xl font-bold">
                                     <AnimateNumber value={3} />+
                                 </span>
-                                <h2 className="text-xl font-medium capitalize text-dark/75">years of experience</h2>
+                                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">years of experience</h2>
                             </div>
                         </div>
 
