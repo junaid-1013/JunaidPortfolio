a15385008e4eee4b89cecf5e1eda0220a54a7481
Junaid Ali Bhatti <junaid.ali101452@gmail.com>
2023-05-14 21:48:53 +0500

    Mobile responsive
    Making is mobile responsive and finishing touches

--- src/components/LiIcon.js
+++ src/components/LiIcon.js
@@ -12,7 +12,7 @@ const LiIcon =({reference})=>{
     
     return(
         <figure className="absolute left-0 stroke-dark dark:stroke-light">
-            <svg className="-rotate-90 -mt-1" width="75" height="75" viewBox="0 0 100 100">
+            <svg className="-rotate-90 -mt-1 md:w-[60px] md:h-[60px] xs:w-[40px] xs:h-[40px] " width="75" height="75" viewBox="0 0 100 100">
                 <circle cx="75" cy="50" r="20" className="stroke-primary dark:stroke-primaryDark stroke-1 fill-none"/>
                 <motion.circle cx="75" cy="50" r="20" className="stroke-[5px] fill-light dark:fill-dark"
                 style={
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,8 @@ import { Montserrat } from 'next/font/google'
 import Head from 'next/head'
 import NavBar from '@/components/NavBar'
 import Footer from '@/components/Footer'
+import { AnimatePresence } from 'framer-motion'
+import { useRouter } from 'next/router'
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -10,6 +12,7 @@ const montserrat = Montserrat({
 })
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
   return (
 
     <>
@@ -18,9 +21,11 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}>
-        <NavBar/>
-        <Component {...pageProps} />
-        <Footer/>
+        <NavBar />
+        <AnimatePresence mode="wait">
+          <Component key={router.asPath} {...pageProps} />
+        </AnimatePresence>
+        <Footer />
       </main>
     </>
 
